f664125908c7d3f0b2ae9fa34331bf9b80f89bd7
a1-creator <asbjorn15johnsen@gmail.com>
2023-10-03 23:38:22 +0200

    Update watchProgressManager.js

--- SERVER/WEBSITE/scripts/JS/watchProgressManager.js
+++ SERVER/WEBSITE/scripts/JS/watchProgressManager.js
@@ -149,6 +149,9 @@ async function SaveWatchProgress(user, progress = 0)
         "KeyToEdit" : "WatchProgress",
         "NewValue" : currentWP,
     }
+    console.log("saved data to server: ")
+    console.log(data)
+
     fetch(`http://${IP_ADDRESS}/editUser`, {
         method: 'POST',
         headers: {
