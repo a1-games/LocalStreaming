cc197fcacd9f39d33620dfddaa0440b419e478d1
a1-creator <asbjorn15johnsen@gmail.com>
2023-10-05 20:56:10 +0200

    removing children instead of temptying inner html on page load

--- SERVER/WEBSITE/scripts/JS/pageManager.js
+++ SERVER/WEBSITE/scripts/JS/pageManager.js
@@ -12,7 +12,11 @@ var currentPageName = "";
 
 function LoadPage(pagename) {
     currentPageName = pagename;
-    contentbox.innerHTML = "";
+    
+    while (contentbox.firstChild) {
+        contentbox.removeChild(contentbox.firstChild);
+    }
+
     $("#main-content-box").load(`${pagename}.html`);
 };
 
