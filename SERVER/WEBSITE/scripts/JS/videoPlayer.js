a95f9d69111b76154c90cbe0576975a0febcf810
a1creator <asbjorn15johnsen@gmail.com>
2024-06-02 12:39:54 +0200

    play/pause buttons are svg now

--- SERVER/WEBSITE/scripts/JS/videoPlayer.js
+++ SERVER/WEBSITE/scripts/JS/videoPlayer.js
@@ -102,13 +102,13 @@ SetEventListener(video, 'click', togglePlay);
 
 // do when video starts playing
 SetEventListener(video, 'play', () => {
-    playpauseButton.style.backgroundImage = "url('../../files/IMAGES/Icons/video_pause.png')";
+    playpauseButton.innerHTML = `<svg fill=\"${textColor}\"` + " class=\"SVG video-button-SVG\" xmlns=\"http://www.w3.org/2000/svg\" id=\"\" viewBox=\"0 0 24 24\" width=\"512\" height=\"512\"><path d=\"M7,0A4,4,0,0,0,3,4V20a4,4,0,0,0,8,0V4A4,4,0,0,0,7,0ZM8,20a1,1,0,0,1-2,0V4A1,1,0,0,1,8,4Z\"/><path d=\"M17,0a4,4,0,0,0-4,4V20a4,4,0,0,0,8,0V4A4,4,0,0,0,17,0Zm1,20a1,1,0,0,1-2,0V4a1,1,0,0,1,2,0Z\"/></svg>";
     infoBox.style.filter = "opacity(0)";
     videoIsPlaying = true;
 });
 // do when video becomes paused
 SetEventListener(video, 'pause', () => {
-    playpauseButton.style.backgroundImage = "url('../../files/IMAGES/Icons/video_play.png')";
+    playpauseButton.innerHTML = `<svg fill=\"${textColor}\"` + " class=\"SVG video-button-SVG\" xmlns=\"http://www.w3.org/2000/svg\" id=\"\" viewBox=\"0 0 24 24\" width=\"512\" height=\"512\"><path d=\"M20.463,7.713l-9.1-6.677A5.317,5.317,0,0,0,2.9,5.323V18.677a5.311,5.311,0,0,0,8.46,4.287l9.105-6.677a5.315,5.315,0,0,0,0-8.574Zm-1.774,6.155-9.1,6.677A2.317,2.317,0,0,1,5.9,18.677V5.323a2.276,2.276,0,0,1,1.27-2.066A2.328,2.328,0,0,1,8.223,3a2.3,2.3,0,0,1,1.362.455l9.1,6.677a2.316,2.316,0,0,1,0,3.736Z\"/><svg>";
     infoBox.style.filter = "";
     videoIsPlaying = false;
 });
